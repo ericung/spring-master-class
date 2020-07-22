24fc9d06e78ac5c58acf34b7573cdf924665e0a2
Eric Ung <ungericw@tutanota.com>
2020-07-21 21:36:45 -0500

    first commit

---
+++ src/main/java/com/udemy/spring/basics/springineasysteps/SpringInEasyStepsApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.basics.springineasysteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInEasyStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInEasyStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/spring/basics/springineasysteps/SpringInEasyStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.basics.springineasysteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInEasyStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
