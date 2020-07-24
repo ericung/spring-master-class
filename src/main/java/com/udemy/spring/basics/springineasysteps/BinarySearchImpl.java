bff18cd03aa62d261e9a3f4f19514233ba97fdbe
Eric Ung <ungericw@tutanota.com>
2020-07-23 21:02:04 -0500

    step 14 forgot qualifier for sort

--- src/main/java/com/udemy/spring/basics/springineasysteps/BinarySearchImpl.java
+++ src/main/java/com/udemy/spring/basics/springineasysteps/BinarySearchImpl.java
@@ -1,17 +1,19 @@
 package com.udemy.spring.basics.springineasysteps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 	@Autowired
-	private SortAlgorithm quickSortAlgorithm;
+	@Qualifier("bubble")
+	private SortAlgorithm sortAlgorithm;
 	
 	public int binarySearch(int[] numbers, int numberToSearchFor)
 	{
-		int[] sortedNumbers = quickSortAlgorithm.sort(numbers);
-		System.out.println(quickSortAlgorithm);
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
 		
 		// Return the result
 		return 3;
