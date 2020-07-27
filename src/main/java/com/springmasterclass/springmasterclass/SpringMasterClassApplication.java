aa0641441f2bddd1926247e09614bfb2aa2241ee
Eric Ung <ungericw@tutanota.com>
2020-07-27 18:29:11 -0500

    main from step 14

--- src/main/java/com/springmasterclass/springmasterclass/SpringMasterClassApplication.java
+++ src/main/java/com/springmasterclass/springmasterclass/SpringMasterClassApplication.java
@@ -16,10 +16,15 @@ public class SpringMasterClassApplication {
 		
 		BinarySearchImpl binarySearch = 
 				applicationContext.getBean(BinarySearchImpl.class);
+	
+		BinarySearchImpl binarySearch1 = 
+				applicationContext.getBean(BinarySearchImpl.class);
+	
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
 		
 		int result = 
 				binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-		
 		System.out.println(result);
 	}
 
